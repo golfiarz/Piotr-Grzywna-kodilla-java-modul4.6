b9a078588f5bf07f4e40b75fcd867ca7632d6f4e
golfiarz <piotrekgrzywna@gmail.com>
2018-01-10 21:41:00 +0100

    Kalkulator

--- src/main/java/com/kodilla/kodillacourse/Kalkulator.java
+++ src/main/java/com/kodilla/kodillacourse/Kalkulator.java
@@ -1,4 +1,13 @@
 package com.kodilla.kodillacourse;
 
 public class Kalkulator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
 }
